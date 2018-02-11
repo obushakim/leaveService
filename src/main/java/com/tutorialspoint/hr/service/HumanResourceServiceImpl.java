293b5f409808858cb18690a8686ddbfdbc5f2a75
obus <irfanhakimobuz@gmail.com>
2018-02-11 23:46:02 +0700

    Initial commit

---
+++ src/main/java/com/tutorialspoint/hr/service/HumanResourceServiceImpl.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint.hr.service;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HumanResourceServiceImpl implements HumanResourceService {
+
+	public void bookLeave(Date startDate, Date endDate, String name) {
+		System.out.println("Booking holiday for [" + startDate + "-" + endDate + "] for [" + name + "] ");
+	}
+}
